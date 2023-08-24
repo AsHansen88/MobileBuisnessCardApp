ccd3f11f98f42d49472eeadf41f7c8a2e127fb9a
AsHansen88 <Ande970s@stud.kea.dk>
2023-08-24 17:34:41 +0200

    :)

--- App.js
+++ App.js
@@ -3,55 +3,41 @@ import { StyleSheet, Text, View, Image, Button, Linking } from 'react-native';
 import React from 'react';
 
 
-
 export default function App() {
-
   const handleEmailPress = () => {
+    Linking.openURL('mailto:ande970s@stud.kea.dk');
+  };
 
-      Linking.openURL('mailto:ande970s@stud.kea.dk');
-    };
-
-    const handleFacebookPress = () => {
-
-      Linking.openURL('https://www.facebook.com/anders.s.hansen.5');
-    };
+  const handleFacebookPress = () => {
+    Linking.openURL('https://www.facebook.com/anders.s.hansen.5');
+  };
 
   return (
+    <View style={styles.container}>
+      <Image source={require('./pictures/Anders.png')} style={styles.image} />
+      <Text style={styles.name}>Anders Hansen</Text>
+      <Text style={styles.jobTitle}>Software Developer</Text>
+      <Text style={styles.email}>Ande970s@stud.kea.dk</Text>
 
-  <View style={styles.container}>
-
-        <Image source={require('./pictures/Anders.png')} style={styles.image} />
-
-        <Text style={styles.name}>Anders Hansen</Text>
-        <Text style={styles.jobTitle}>Software Developer</Text>
-        <Text style={styles.email}>Ande970s@stud.kea.dk</Text>
-
-        <View style={styles.buttonContainer}>
-                <Button title="Email" onPress={handleEmailPress} />
-                <Button title="Facebook" onPress={handleFacebookPress} />
-
-              </View>
-        <Text style={styles.phone}>50405381</Text>
-
-<Text style={styles.about}>
-<h2> About </h2>
-        First app created by android student at kea
-      </Text>
-
-<Text style={styles.interests}>
-
-<h2> Interests </h2>
-        Games, internet fanatic, programming, documentary etc.
-      </Text>
-
+      <View style={styles.buttonContainer}>
+        <Button title="Email" onPress={handleEmailPress} />
+        <Button title="Facebook" onPress={handleFacebookPress} />
       </View>
 
+      <Text style={styles.phone}>50405381</Text>
 
+      <Text style={styles.sectionTitle}>About</Text>
+      <Text style={styles.sectionText}>
+        First app created by an android student at KEA
+      </Text>
 
-)
+      <Text style={styles.sectionTitle}>Interests</Text>
+      <Text style={styles.sectionText}>
+        Games, internet fanatic, programming, documentaries, etc.
+      </Text>
+    </View>
+  );
 }
-
-//CSS
 
 const styles = StyleSheet.create({
   container: {
@@ -59,57 +45,49 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-
     padding: 20,
-        backgroundColor: '#f0f0f0',
-        borderRadius: 10,
-        margin: 20,
-      },
-      name: {
-        fontSize: 24,
-        fontWeight: 'bold',
-      },
-      jobTitle: {
-        fontSize: 18,
-        color: '#666',
-      },
-      email: {
-        fontSize: 16,
-        color: '#333',
-      },
-
-      buttonContainer: {
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          marginTop: 10,
-        },
- about: {
-    fontSize: 16,
-    color: '#333',
-    marginTop: 10,
-    textAlign: 'center',
-},
-
-interests: {
-    fontSize: 16,
-    color: '#333',
-    marginTop: 10,
-    textAlign: 'center',
-},
-
-      phone: {
-        fontSize: 16,
-        color: '#333',
-        marginTop: 5,
-
+    backgroundColor: '#f0f0f0',
+    borderRadius: 10,
+    margin: 20,
   },
-
+  name: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  jobTitle: {
+    fontSize: 18,
+    color: '#666',
+  },
+  email: {
+    fontSize: 16,
+    color: '#333',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10,
+  },
+  phone: {
+    fontSize: 16,
+    color: '#333',
+    marginTop: 5,
+  },
   image: {
-    width: 100, // Adjust the width as needed
-    height: 100, // Adjust the height as needed
-    borderRadius: 50, // Make it a circle
+    width: 100,
+    height: 100,
+    borderRadius: 50,
     marginBottom: 10,
   },
-
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+  sectionText: {
+    fontSize: 16,
+    color: '#333',
+    marginTop: 10,
+    textAlign: 'center',
+  },
 });
 
